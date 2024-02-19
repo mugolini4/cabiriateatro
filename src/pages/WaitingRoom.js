2478f8ea7c2158fa7b354ced5ab87119d96a9d6b
Matilde Ugolini <matilde.ugolini@gmail.com>
2024-02-19 21:54:05 +0100

    cover change

--- src/pages/WaitingRoom.js
+++ src/pages/WaitingRoom.js
@@ -36,7 +36,7 @@ const WaitingRoom = () => {
             <Typography gutterBottom variant={"h4"} color={`gray`} paddingX={1}>
                 {showData?.name}
             </Typography>
-            <img src={'/cover_reggio.jpeg'} width={'210px'}
+            <img src={'/cover_r_g.jpeg'} width={'210px'}
                  style={{
                      filter: `opacity(0.75)`,
                      borderRadius: '20px',
